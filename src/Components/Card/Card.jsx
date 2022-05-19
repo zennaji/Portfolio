fa752afc47f2d32bd59bb3b4caebb77697681fe4
ZakariaEnnaji <zakaria.ennaji@outlook.com>
2022-05-20 01:06:11 +0200

    card design is changed

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { icons } from 'react-icons/lib'
 import './Card.css'
-const Card = ({emoji, heading, detail}) => {
+const Card = ({titleIcon, heading, detail, icons}) => {
+ 
   return (
     <div className="card">
-        <img src={emoji} alt="" />
-        <span>{heading}</span>
-        <span>{detail}</span>
+        {/* <img src={emoji} alt="" /> */}
+        <div className='card_icon'>{titleIcon}</div>
+        <span >{heading}</span>
+        <div className="card_icons">{icons}</div>
+        <div className='cardText'>{detail}</div>
         {/* <button className="c-button">LEARN MORE</button> */}
     </div>
   )
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -5,7 +5,37 @@ import HeartEmoji from "../../img/heartemoji.png"
 import Humble from "../../img/humble.png"
 import Card from '../Card/Card'
 import Resume from "./Resume.pdf"
+import {FaLaptopCode} from 'react-icons/fa'
+import {FaWrench} from 'react-icons/fa'
+import {FaServer} from 'react-icons/fa'
+// import icons
+import {SiHtml5} from 'react-icons/si'
+import {SiCss3} from 'react-icons/si'
+import {SiJavascript} from 'react-icons/si'
+import {SiSass} from 'react-icons/si'
+import {SiDart} from 'react-icons/si'
+import {SiFlutter} from 'react-icons/si'
+import {SiReact} from 'react-icons/si'
+import {SiTypescript} from 'react-icons/si'
+import {SiPhp} from 'react-icons/si'
+import {SiMysql} from 'react-icons/si'
+import {SiSymfony} from 'react-icons/si'
+import {SiNodedotjs} from 'react-icons/si'
+import {SiExpress} from 'react-icons/si'
+import {SiMongodb} from 'react-icons/si'
+import {SiNpm} from 'react-icons/si'
+import {SiGit} from 'react-icons/si'
+import {SiGithub} from 'react-icons/si'
+import {SiVisualstudiocode} from 'react-icons/si'
+import {SiStackoverflow} from 'react-icons/si'
+import {SiAdobephotoshop} from 'react-icons/si'
+import {SiAdobexd} from 'react-icons/si'
+import {SiAdobeillustrator} from 'react-icons/si'
+import {SiFigma} from 'react-icons/si'
+import {SiAdobepremierepro} from 'react-icons/si'
+import {FaCheck} from 'react-icons/fa'
 
+ 
 import {themeContext} from "./../../Context";
 import {useContext} from "react";
 
@@ -25,8 +55,9 @@ const Services = () => {
             <span style={{color: darkMode ? "#fff" : "",}}>SKILLS & </span>
             <span>KNOWLEDGE </span>
             <span>
-              I’m familiar with a variety of modern web technologies and <br />
-              programming languages and I’m always looking for new <br />
+            Turning coffee into code since 2019! I'm a highly driven, <br />
+            great web and app developer with a thirst for self-improvement <br />
+            and learning new tech. Here's some of what I can do:
             </span>
             <a href={Resume} download>
 
@@ -43,9 +74,14 @@ const Services = () => {
               
               style={{left:"2rem", top:"12rem"}}>
                   <Card
-                    emoji = {HeartEmoji}
+                    titleIcon = {<FaLaptopCode/>}
+                    icons = {[<SiHtml5/>, <SiCss3/>, <SiJavascript/>, <SiSass/>,<SiDart/>, <SiFlutter/>, <SiReact/>, <SiTypescript/>]}
                     heading = {'Front-End'}
-                    detail = {"HTML, (S)CSS, JS, Dart, Flutter, TS and React"}
+                    detail = {[<FaCheck className='check'/>," Fully responsive websites and apps that look good on any device or screen.", <br/>,
+                    <FaCheck className='check'/>, " Intuitive UX/UI design using a variety of libraries and the latest HTML5 & CSS3 standards.", <br/>,
+                    <FaCheck className='check'/>, " Intuitive UX/UI design using a variety of libraries and the latest HTML5 & CSS3 standards."
+
+                    ]}
                   />
               </motion.div>
               {/* second card */}
@@ -56,9 +92,14 @@ const Services = () => {
               
               style={{left:"25rem", top: "-10rem"}}>
                   <Card
-                    emoji = {Glasses}
+                    titleIcon = {<FaServer/>}
+                    icons = {[<SiPhp/>, <SiMysql/>, <SiSymfony/>, <SiNodedotjs/>, <SiExpress/>, <SiMongodb/>]}
                     heading = {'Back-End'}
-                    detail = {"PHP, MySQL, Symfony, NodeJS, Express and mongoDB"}
+                    detail = {[
+                    <FaCheck className='check'/>, " User authentication; multi-layered middleware; complex route-handling and more.", <br/>,
+                    <FaCheck className='check'/>, " Persistant and secure data storage solutions using SQL, MongoDB or Firebase.", <br/>,
+                    <FaCheck className='check'/>, " Custom-built RESTful APIs and servers powered by Javascript", <br/>,
+                  ]}
                   />
               </motion.div>
                {/* 3rde card */}
@@ -71,9 +112,14 @@ const Services = () => {
               style={{left:"20rem",  top:"20rem" }}
               >
                   <Card
-                    emoji = {Humble}
-                    heading = {'UI/UX'}
-                    detail = {"Adobe: Photoshop, illustrator, XDesign, Premier pro, figma"}
+                    titleIcon = {<FaWrench/>}
+                    icons = {[<SiNpm/>, <SiGit/>, <SiGithub/>, <SiVisualstudiocode/>, <SiStackoverflow/>, <SiAdobephotoshop/>, <SiAdobexd/>, <SiAdobeillustrator/>,]}
+                    heading = {'Tools'}
+                    detail = {[
+                      <FaCheck className='check'/>, " Git / Github for version control, collaborative working and ensuring updates are smooth.", <br/>,
+                      <FaCheck className='check'/>, " Integrating reusable code for speed and efficiency using libraries and other published packages.", <br/>,
+                      <FaCheck className='check'/>, " Web-app deployment including testing, hosting, DNS configuration and more.", <br/>,
+                    ]}
                   />
               </motion.div>
               <div className="blur s-blur2" style={{background: "#ABF1FF94"}}></div>
